98280b1a0058816b1982fbda3dbd3aa8ee46eb26
stephanie-vm <stephaniecetav@gmail.com>
2021-12-08 10:36:58 -0600

    Agrego archivo redirects

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import StudentList from './routes/StudentList'
-import Student from './routes/Student'
-import StudentForm from './routes/StudentForm'
+// import StudentList from './routes/StudentList'
+// import Student from './routes/Student'
+// import StudentForm from './routes/StudentForm'
 import { AlertProvider } from './hooks/useAlert';
 
 ReactDOM.render(
