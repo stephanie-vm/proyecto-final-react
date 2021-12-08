a4298d6576b1581544b996c21d21dd8660b8d07c
stephanie-vm <stephaniecetav@gmail.com>
2021-12-08 15:11:33 -0600

    Arreglo error

--- src/routes/Student.js
+++ src/routes/Student.js
@@ -1,40 +1,43 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react"
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { useEffect } from "react/cjs/react.development"
 
-export default function Student() {
-    const [student, setStudent] = useState({});
-    const [isLoaded, setIsLoaded] = useState(false);
-    const params = useParams();
+const Student = () => {
+    const [student, setStudent] = useState({})
+    const [isLoaded, setIsLoaded] = useState(false)
+    const params = useParams()
 
     const getStudentById = (id) => {
-    fetch(`${process.env.REACT_APP_API_URL}/student/${id}`, {
-    method: "GET",
-    headers: {
-        "x-hasura-admin-secret":
-            `${process.env.REACT_APP_HASURA_SECRET}`,
-    },
-    })
-        .then((response) => response.json())
-        .then((result) => {
-        setIsLoaded(true);
-        setStudent(result.students_by_pk);
-        });
-};
+        fetch(`${process.env.REACT_APP_API_URL}/student/${id}`, {
+        method: 'GET',
+        headers: {
+            'x-hasura-admin-secret': `${process.env.REACT_APP_HASURA_SECRET}`
+        }
+        }).then(response => response.json())
+        .then(result => {
+            setIsLoaded(true)
+            setStudent(result.students_by_pk)
+        })
+    }
 
-    useEffect(() => {
-    getStudentById(params.StudentId);
-    }, [isLoaded]);
-
-    if (!isLoaded) return <p>Loading...</p>;
+    useEffect(()=>{
+        getStudentById(params.StudentId)
+    }, [isLoaded])
+    
+    if (!isLoaded) return <p>Loading...</p>
 
     return (
-    <>
-        <h1>Student Profile</h1>
-        <hr />
-        <h2>Name:</h2>
-        <span>{student?.name}</span>
-        <h2>ID</h2>
-        <span>{params.StudentId}</span>
-    </>
-    );
+        <>
+            <h1>Student Profile</h1>
+            <Link to={`updateStudent/${params.StudentId}`}>Update Student</Link>
+            <hr />
+            <h2>Name:</h2>
+            <span>{student?.name}</span>
+            <h3>ID</h3>
+            <span>{params.StudentId}</span>
+        </>
+    )
 }
+
+export default Student
